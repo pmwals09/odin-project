21b98ff54b0423afce93b3c42e6f1a3cc3326873
Pat Walsh <pmwals09@gmail.com>
2022-03-14 21:36:22 -0400

    RSpec Testing

---
+++ ruby-on-rails/ruby-programming/ruby-exercises/ruby_basics/ruby_testing_repo/lib/14_find_number.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# class for computer to find random number
+class FindNumber
+  attr_reader :min, :max, :answer, :guess
+
+  def initialize(min, max, answer = RandomNumber.new(min, max), guess = nil)
+    @min = min
+    @max = max
+    # The RandomNumber class will have an instance variable '@value'
+    @answer = answer.value
+    @guess = guess
+  end
+
+  def make_guess
+    (@min + @max) / 2
+  end
+
+  def game_over?
+    @guess == @answer
+  end
+
+  def update_range
+    @min = guess + 1 if guess < answer
+    @max = guess - 1 if guess > answer
+  end
+end
